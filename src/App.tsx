e5d30ef17dfea262172e2a054bdd98caec6b466f
NavasardianMichael <navasardianmichael@gmail.com>
2025-11-13 05:42:49 +0400

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,155 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+
+const CHRONICLE_DISPLAY_FONT_TEMPLATE = [
+  {
+    label: 'Chronicle Display Roman',
+    fontWeight: 400,
+    fontStyle: 'normal',
+  },
+  {
+    label: 'Chronicle Display Light Italic',
+    fontWeight: 300,
+    fontStyle: 'italic',
+  },
+  {
+    label: 'Chronicle Display Semibold',
+    fontWeight: 600,
+    fontStyle: 'normal',
+  },
+  {
+    label: 'Chronicle Display Black',
+    fontWeight: 900,
+    fontStyle: 'normal',
+  },
+]
+
+const CENTRA_NO2_FONT_TEMPLATE = [
+  {
+    label: 'Centra No2',
+    fontWeight: 400,
+    fontStyle: 'normal',
+  },
+  {
+    label: 'Centra No2 Italic',
+    fontWeight: 400,
+    fontStyle: 'italic',
+  },
+  {
+    label: 'Centra No2 Bold',
+    fontWeight: 700,
+    fontStyle: 'normal',
+  },
+  {
+    label: 'Centra No2 Bold Italic',
+    fontWeight: 700,
+    fontStyle: 'italic',
+  },
+]
 
 const App = () => {
-  const [fontSize, setFontSize] = useState(16);
+  const [fontSize, setFontSize] = useState(16)
+  const [chronicleDisplayCommonText, setChronicleDisplayCommonText] =
+    useState('')
+  const [centraNo2CommonText, setCentraNo2CommonText] = useState('')
 
   return (
-    <div className="flex flex-col gap-8 p-8">
-      <h1 className="text-2xl font-bold">GAP UI Storybook</h1>
-      <div>
-        <label className="block mb-2 text-sm font-medium text-gray-900">
-          Font Size: {fontSize}px
-        </label>
-        <input
-          type="range"
-          min="10"
-          max="30"
-          value={fontSize}
-          onChange={(e) => setFontSize(Number(e.target.value))}
-          className="w-full max-w-3xl"
-        />
+    <div className='flex flex-col gap-8 p-8'>
+      <h1 className='font-bold text-6xl text-center'>GAP UI Storybook</h1>
+      <hr />
+      <div className='flex flex-col gap-4 chronicle-display'>
+        <h2 className='text-5xl'>Chronicle Display</h2>
+        <div className='flex flex-row gap-8'>
+          <div className='flex flex-col gap-8 w-1/3'>
+            <div className='w-full'>
+              <label className='block mb-2 text-sm'>
+                Custom Chronicle Display Text:
+              </label>
+              <input
+                value={chronicleDisplayCommonText}
+                onChange={(e) => setChronicleDisplayCommonText(e.target.value)}
+                className='w-full border border-gray-300 rounded px-3 py-2'
+              />
+            </div>
+            <div className='w-full'>
+              <label className='block mb-2 text-sm'>
+                Font Size: {fontSize}px
+              </label>
+              <input
+                type='range'
+                min='10'
+                max='50'
+                value={fontSize}
+                onChange={(e) => setFontSize(Number(e.target.value))}
+                className='w-full'
+              />
+            </div>
+          </div>
+          <div className='flex flex-col gap-4 border p-4 grow rounded-sm'>
+            {CHRONICLE_DISPLAY_FONT_TEMPLATE.map((font) => (
+              <p
+                key={font.label}
+                style={{
+                  fontFamily: 'Chronicle Display, serif',
+                  fontWeight: font.fontWeight,
+                  fontStyle: font.fontStyle,
+                  fontSize: `${fontSize}px`,
+                }}
+              >
+                {chronicleDisplayCommonText || font.label}
+              </p>
+            ))}
+          </div>
+        </div>
       </div>
-      <div className="p-4 border rounded" style={{ fontSize: `${fontSize}px` }}>
-        This is a sample text to demonstrate dynamic font sizing using Tailwind
-        CSS and React.
+      <hr />
+      <div className='flex flex-col gap-4 centra-no2'>
+        <h2 className='text-5xl'>Centra No2</h2>
+        <div className='flex flex-row gap-8'>
+          <div className='flex flex-col gap-8 w-1/3'>
+            <div className='w-full'>
+              <label className='block mb-2 text-sm'>
+                Custom Centra No2 Text:
+              </label>
+              <input
+                value={centraNo2CommonText}
+                onChange={(e) => setCentraNo2CommonText(e.target.value)}
+                className='w-full border border-gray-300 rounded px-3 py-2'
+              />
+            </div>
+            <div className='w-full'>
+              <label className='block mb-2 text-sm'>
+                Font Size: {fontSize}px
+              </label>
+              <input
+                type='range'
+                min='10'
+                max='50'
+                value={fontSize}
+                onChange={(e) => setFontSize(Number(e.target.value))}
+                className='w-full'
+              />
+            </div>
+          </div>
+          <div className='flex flex-col gap-4 border p-4 grow rounded-sm'>
+            {CENTRA_NO2_FONT_TEMPLATE.map((font) => (
+              <p
+                key={font.label}
+                style={{
+                  fontFamily: 'Centra No2, sans-serif',
+                  fontWeight: font.fontWeight,
+                  fontStyle: font.fontStyle,
+                  fontSize: `${fontSize}px`,
+                }}
+              >
+                {centraNo2CommonText || font.label}
+              </p>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
