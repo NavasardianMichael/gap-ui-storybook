27870d95971e24b7a9a2261e5b012ee3b8d0b523
NavasardianMichael <navasardianmichael@gmail.com>
2025-11-25 18:05:39 +0400

    fonts

---
+++ src/demo/constants/fonts.ts
@@ -0,0 +1,140 @@
+export const CHRONICLE_DISPLAY_FONT_TEMPLATE = [
+  {
+    label: "Chronicle Display XLight",
+    fontWeight: 200,
+    fontStyle: "normal",
+  },
+  {
+    label: "Chronicle Display XLight Italic",
+    fontWeight: 200,
+    fontStyle: "italic",
+  },
+  {
+    label: "Chronicle Display Light",
+    fontWeight: 300,
+    fontStyle: "normal",
+  },
+  {
+    label: "Chronicle Display Light Italic",
+    fontWeight: 300,
+    fontStyle: "italic",
+  },
+  {
+    label: "Chronicle Display Roman",
+    fontWeight: 400,
+    fontStyle: "normal",
+  },
+  {
+    label: "Chronicle Display Italic",
+    fontWeight: 400,
+    fontStyle: "italic",
+  },
+  {
+    label: "Chronicle Display Semibold",
+    fontWeight: 600,
+    fontStyle: "normal",
+  },
+  {
+    label: "Chronicle Display Semibold Italic",
+    fontWeight: 600,
+    fontStyle: "italic",
+  },
+  {
+    label: "Chronicle Display Bold",
+    fontWeight: 700,
+    fontStyle: "normal",
+  },
+  {
+    label: "Chronicle Display Bold Italic",
+    fontWeight: 700,
+    fontStyle: "italic",
+  },
+  {
+    label: "Chronicle Display Black",
+    fontWeight: 900,
+    fontStyle: "normal",
+  },
+  {
+    label: "Chronicle Display Black Italic",
+    fontWeight: 900,
+    fontStyle: "italic",
+  },
+];
+
+export const CENTRA_NO2_FONT_TEMPLATE = [
+  {
+    label: "Centra No2 Hairline",
+    fontWeight: 100,
+    fontStyle: "normal",
+  },
+  {
+    label: "Centra No2 Hairline Italic",
+    fontWeight: 100,
+    fontStyle: "italic",
+  },
+  {
+    label: "Centra No2 Thin",
+    fontWeight: 200,
+    fontStyle: "normal",
+  },
+  {
+    label: "Centra No2 Thin Italic",
+    fontWeight: 200,
+    fontStyle: "italic",
+  },
+  {
+    label: "Centra No2 Light",
+    fontWeight: 300,
+    fontStyle: "normal",
+  },
+  {
+    label: "Centra No2 Light Italic",
+    fontWeight: 300,
+    fontStyle: "italic",
+  },
+  {
+    label: "Centra No2 Book",
+    fontWeight: 400,
+    fontStyle: "normal",
+  },
+  {
+    label: "Centra No2 Book Italic",
+    fontWeight: 400,
+    fontStyle: "italic",
+  },
+  {
+    label: "Centra No2 Medium",
+    fontWeight: 500,
+    fontStyle: "normal",
+  },
+  {
+    label: "Centra No2 Medium Italic",
+    fontWeight: 500,
+    fontStyle: "italic",
+  },
+  {
+    label: "Centra No2 Bold",
+    fontWeight: 700,
+    fontStyle: "normal",
+  },
+  {
+    label: "Centra No2 Bold Italic",
+    fontWeight: 700,
+    fontStyle: "italic",
+  },
+  {
+    label: "Centra No2 ExtraBold",
+    fontWeight: 800,
+    fontStyle: "normal",
+  },
+  {
+    label: "Centra No2 ExtraBold Italic",
+    fontWeight: 800,
+    fontStyle: "italic",
+  },
+  {
+    label: "Centra No2 Black",
+    fontWeight: 900,
+    fontStyle: "normal",
+  },
+];
